693f836fb593db4a671332474bd03fb8cf2e25e8
Raj72620 <nishanthraj9618@gmail.com>
2026-05-22 19:46:17 +0530

    graph

---
+++ Graphs/alienDictionary.java
@@ -0,0 +1,58 @@
+package Graphs;
+
+import java.util.*;
+
+public class alienDictionary {
+
+    public static String findOrder(String[] disct,int N , int k){
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+        for(int i=0;i<k;i++){
+            graph.add(new ArrayList<>());
+        }
+        for(int i=0;i<N-1;i++){
+            String s1 = disct[i];
+            String s2 = disct[i+1];
+            int len = Math.min(s1.length(), s2.length());
+
+            for(int j=0;j<len;j++){
+                if(s1.charAt(j)!=s2.charAt(j)){
+                    int u = s1.charAt(j)-'a';
+                    int v = s2.charAt(j)-'a';
+                    graph.get(u).add(v);
+                    break;
+                }
+            }
+        }
+        int[] inDegree = new int[k];
+        for(int i=0;i<k;i++){
+            for(int neig : graph.get(i)){
+                inDegree[neig]++;
+            }
+        }
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0;i<k;i++){
+            if(inDegree[i]==0){
+                q.add(i);
+            }
+        }
+        StringBuilder ans = new StringBuilder();
+        while(!q.isEmpty()){
+            int node = q.remove();
+            ans.append((char)(node+'a'));
+            for(int neig : graph.get(node)){
+                inDegree[neig]--;
+                if(inDegree[neig]==0){
+                    q.add(neig);
+                }
+            }
+        }
+        return ans.toString();
+    }
+    public static void main(String[] args) {
+        String[] disct = {"baa","abcd","abca","cab","cad"};
+        int k=4;
+        int N=disct.length;
+        String ans = findOrder(disct, N, k);
+        System.out.println(ans);
+    }
+}
--- practice.java
+++ practice.java
@@ -1,26 +1,44 @@
 import java.util.*;
-
-
 public  class practice{
-    
-    public static boolean preRequisites(int[][] arr , int numCourses){
-        ArrayList<Integer>[] graph = new ArrayList[numCourses];
 
-        for(int i=0;i<graph.length;i++){
-            graph[i]= new ArrayList<>();
-        }
-        int[] inDegree = new int[numCourses];
-        
-        for(int[] ele : arr){
-            int courses = ele[0];
-            int prereq = ele[1];
+    public static boolean topoSort(int[][] edges , int V){
 
-            graph[prereq].add(courses);
-            inDegree[courses]++;
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+        for(int i=0;i<V;i++){
+            graph.add(new ArrayList<>());
         }
+        int[] indegree= new int[V];
+        for(int[] ed : edges){
+            int u=ed[0];
+            int ve = ed[1];
+
+            graph.get(u).add(ve);
+            indegree[ve]++;
+        }
+        Queue<Integer>q = new LinkedList<>();
+        for(int i=0;i<indegree.length;i++){
+            if(indegree[i]==0){
+                q.add(i);
+            }
+        }
+        while(!q.isEmpty()){
+            int curr = q.remove();
+           for(int neig : graph.get(curr)){
+            indegree[neig]--;
+            if(indegree[neig]==0){
+                q.add(neig);
+            }
+           }
+        }
+         for(int i=0;i<indegree.length;i++){
+            if(indegree[i]!=0) return false;
+           }
+        return true;
     }
-    public static void main(String[] args) {
-        int[][] arr={{1,0},{2,0},{3,1},{3,2}};
-        int numCourses=4;
+
+  public static void main(String[] args) {
+    int[][] edges = {{1, 3}, {2, 3}, {4, 1}, {4, 0}, {5, 0}, {5, 2}};
+    int V=6;
+    System.out.println(topoSort(edges, V));
     }
 }
