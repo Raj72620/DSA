62f6f81531aad45cd7c3bcfc614888427b537c43
Raj72620 <nishanthraj9618@gmail.com>
2026-06-06 16:31:48 +0530

    graph

---
+++ Graphs/MinimumSpanningTree/PrimsAlgo.java
@@ -0,0 +1,70 @@
+package Graphs.MinimumSpanningTree;
+import java.util.*;
+
+public class PrimsAlgo {
+
+    static class pairs{
+   
+        int dest;
+        int weight;
+
+        public pairs(int dest,int weight) {
+            this.dest=dest;
+            this.weight=weight;
+        }
+    }
+static class Info{
+    int node ;
+    int wt;
+    int parent;
+
+        public Info(int node , int wt, int parent) {
+            this.node=node;
+            this.wt=wt;
+            this.parent=parent;
+        }
+    
+    }
+    public static int getMin(int v ,ArrayList<ArrayList<pairs>> graph){
+        PriorityQueue<Info>pq = new PriorityQueue<>((a,b)->a.wt-b.wt);
+        boolean[] visited = new boolean[v];
+        pq.add(new Info(0, 0, -1));
+                int sum=0;
+
+        while(!pq.isEmpty()){
+            Info info = pq.remove();
+            int node = info.node;
+            int wt=info.wt;
+            int par=info.parent;
+
+            if(visited[node]) continue;
+
+            visited[node]=true;
+            sum+=wt;
+
+            for(pairs neig : graph.get(node)){
+                if(!visited[neig.dest]){
+                    pq.add(new Info(neig.dest, neig.weight, node));
+                }
+            }
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+         int v= 5;
+      int[][] arr = {{0, 1, 1},{0, 2, 1}, {1, 0, 2},{1, 2, 1},{2, 0, 1},{2, 1, 1},{2, 3, 2},{2, 4, 2},{3, 2, 2},{3, 4, 1},{4, 2, 2},{4, 3, 1}
+};
+        ArrayList<ArrayList<pairs>> graph = new ArrayList<>();
+        for(int i=0;i<v;i++){
+            graph.add(new ArrayList<>());
+        }
+        for(int[] edge : arr){
+            int u=edge[0];
+            int vx=edge[1];
+            int wt=edge[2];
+
+            graph.get(u).add(new pairs(vx, wt));
+        }
+        System.out.println(getMin(v, graph));
+    }
+}
--- Graphs/graphPractice.java
+++ Graphs/graphPractice.java
@@ -5,53 +5,79 @@ import java.util.*;
 public class graphPractice {
 
     static class Pairs {
-        int src;
+       
         int dest;
         int wt;
 
-        Pairs(int src, int dest, int wt) {
-            this.src = src;
+        Pairs(int dest, int wt) {
+      
             this.dest = dest;
             this.wt = wt;
         }
     }
 
-    public static void insert(int v, ArrayList<Pairs>[] graph) {
+    public static int getMin(int v , ArrayList<ArrayList<Pairs>> graph,int sr){
+            PriorityQueue<Pairs> pq = new PriorityQueue<>((a,b)->a.wt-b.wt);
+            boolean[] visited = new boolean[v];
+         
+            pq.add(new Pairs(sr, 0));
+            int sum=0;
 
-        graph[0].add(new Pairs(0, 1, 1));
-        graph[0].add(new Pairs(0, 2, 2));
+            while(!pq.isEmpty()){
+          
+                Pairs curr = pq.remove();
 
-        graph[1].add(new Pairs(1, 0, 3));
-        graph[1].add(new Pairs(1, 3, 3));
+                int node = curr.dest;
+                int weight = curr.wt;
 
-         graph[2].add(new Pairs(2, 0, 3));
-          graph[2].add(new Pairs(2, 3, 3));
-           graph[2].add(new Pairs(2, 4, 3));
+                if(visited[node]) continue;
 
-        graph[3].add(new Pairs(3, 1, 4));
-        graph[3].add(new Pairs(3, 2, 4));
-         graph[3].add(new Pairs(3, 4, 4));
+                visited[node]=true;
+                sum+=weight;
+                
+                for(Pairs neig : graph.get(node)){
 
-         graph[4].add(new Pairs(4, 2, 4));
-        graph[4].add(new Pairs(4, 3, 4));
-    }
-
-    public static void main(String[] args) {
-
-        int v = 5;
-        ArrayList<Pairs>[] graph = new ArrayList[v];
-        for (int i = 0; i < v; i++) {
-            graph[i] = new ArrayList<>();
-        }
-        insert(v, graph);
-
-        for (int i = 0; i < v; i++) {
-            System.out.print(i + " -> ");
-
-            for (Pairs e : graph[i]) {
-                System.out.print("(" + e.dest + "," + e.wt + ") ");
+                    int AdjNode = neig.dest;
+                    int wt = neig.wt;
+                    
+                  if(!visited[AdjNode]){
+                    pq.add(new Pairs(AdjNode, wt));
+                  }
+                }
             }
-            System.out.println();
+            return sum;
+    }
+    public static void main(String[] args) {
+        int v= 5;
+      int[][] arr = {
+    {0, 1, 1},
+    {0, 2, 1},
+
+    {1, 0, 2},
+    {1, 2, 1},
+
+    {2, 0, 1},
+    {2, 1, 1},
+    {2, 3, 2},
+    {2, 4, 2},
+
+    {3, 2, 2},
+    {3, 4, 1},
+
+    {4, 2, 2},
+    {4, 3, 1}
+};
+        ArrayList<ArrayList<Pairs>> graph = new ArrayList<>();
+        for(int i=0;i<v;i++){
+            graph.add(new ArrayList<>());
         }
+        for(int[] edge : arr){
+            int u=edge[0];
+            int vx=edge[1];
+            int wt=edge[2];
+
+            graph.get(u).add(new Pairs(vx, wt));
+        }
+        System.out.println(getMin(v, graph, 0));
     }
 }
