d761db75b445c2f385e5436b96f4e61ed7d323cf
Raj72620 <nishanthraj9618@gmail.com>
2026-06-10 13:16:58 +0530

    dp

--- DP/oneDimensionalDP/climbingStairs.java
+++ DP/oneDimensionalDP/climbingStairs.java
@@ -1,15 +1,24 @@
 package DP.oneDimensionalDP;
+import java.util.*;
 
 public class climbingStairs {
 
-    public static int findTotalWays(int n){
+    public static int findTotalWays(int n,int[] dp){
+       
         if(n==0 || n==1) return 1;
+        
+        if(dp[n]!=-1) return dp[n];
 
-        return findTotalWays(n-1)+findTotalWays(n-2);
+       int left = findTotalWays(n-1, dp);
+       int right = findTotalWays(n-2, dp);
 
+       dp[n]= left+right;
+       return dp[n];
     }
     public static void main(String[] args) {
         int n=3;
-        System.out.println(findTotalWays(n));
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        System.out.println(findTotalWays(n,dp));
     }
 }
