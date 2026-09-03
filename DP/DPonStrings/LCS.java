a8316a68ad1ec22784618dfc166c1fb1148e4ebb
Raj72620 <nishanthraj9618@gmail.com>
2026-09-03 20:20:21 +0530

    DP

---
+++ DP/DPonStrings/LCS.java
@@ -0,0 +1,48 @@
+package DP.DPonStrings;
+
+//Longest common subsequence
+
+public class LCS {
+
+    //Recursion
+
+    public static int solve(String s1, String s2, int idx1, int idx2){
+        if(idx1<0 || idx2<0) return 0;
+
+        if(s1.charAt(idx1)==s2.charAt(idx2)){
+            return 1+solve(s1, s2, idx1-1, idx2-1);
+        }
+        return Math.max(solve(s1, s2, idx1-1, idx2), solve(s1, s2, idx1, idx2-1));
+        }
+
+    // Memoization
+
+    public static int memo(String s1, String s2, int[][] dp,int idx1, int idx2){
+
+        if(idx1<0 || idx2<0) return 0;
+
+        if(dp[idx1][idx2]!=-1) {
+            return dp[idx1][idx2];
+        }
+        if(s1.charAt(idx1)==s2.charAt(idx2)){
+            return dp[idx1][idx2]+1+memo(s1, s2, dp, idx1-1, idx2-1);
+        }
+            return dp[idx1][idx2]+ Math.max(memo(s1, s2, dp, idx1-1, idx2), memo(s1, s2, dp, idx1, idx2-1));
+    }
+    public static void main(String[] args) {
+        String s1= "ABCBDAB";
+        String s2="BDCABA";
+        int n= s1.length();
+        int m= s2.length();
+
+        int[][] dp = new int[n][m];
+        for(int[] row : dp){
+        java.util.Arrays.fill(row,-1);
+        }
+
+       System.out.println( solve(s1,s2,n-1,m-1));
+
+       System.out.println(memo(s1, s2, dp, n-1, m-1));
+
+    }
+}
--- DP/subSeqAndSubsets/zeroOneKnapsack.java
+++ DP/subSeqAndSubsets/zeroOneKnapsack.java
@@ -17,7 +17,59 @@ public class zeroOneKnapsack {
     }
 
     //Memoizatio
-    
+    public static int Memo(int[] wg, int[] val, int idx, int bag){
+        int n= wg.length;
+        int[][] dp= new int[n][bag+1];
+        for(int[] row : dp){
+            java.util.Arrays.fill(row,-1);
+        }
+       int ans= solveMemo(wg,val,dp,n,idx,bag);
+       return ans;
+    }
+    public static int solveMemo(int[] wg,int[] val,int[][] dp,int n, int idx, int bag){
+
+        if(idx==0){
+            if(wg[0]<=bag){
+                return val[0];
+            }
+            return 0;
+        }
+        if(dp[idx][bag]!=-1){
+            return dp[idx][bag];
+        }
+        int notPick = solveMemo(wg, val,dp,n, idx-1, bag);
+
+        int pick=Integer.MIN_VALUE;
+        if(wg[idx]<=bag){
+            pick=val[idx]+solveMemo(wg, val,dp,n, idx-1, bag-wg[idx]);
+        }
+        dp[idx][bag]=Math.max(notPick, pick);
+
+        return dp[idx][bag];
+    } 
+
+    // Tabulation
+
+    public static int tab(int[] wg, int[] val,int n,int bag){
+
+        int[][] dp=new int[n][bag+1];
+        for(int i=wg[0];i<=bag;i++){
+            dp[0][i]=val[0];
+        }
+
+        for(int idx=1;idx<n;idx++){
+            for(int cap=0;cap<=bag;cap++){
+
+                int notPick=dp[idx-1][cap];
+                int pick=Integer.MIN_VALUE;
+                if(wg[idx]<=cap){
+                    pick=val[idx]+dp[idx-1][cap-wg[idx]];
+                }
+                dp[idx][cap]=Math.max(pick, notPick);
+            }
+        }
+        return dp[n-1][bag];
+    }
     public static void main(String[] args) {
         int[] weight={1,2,4,5};
         int[] values={1,4,5,7};
@@ -26,5 +78,7 @@ public class zeroOneKnapsack {
         int n=weight.length;
 
        System.out.println( findTheMaxProfit(weight,values,n-1,bagCapacity));
+       System.out.println(Memo(weight, values, n-1, bagCapacity));
+       System.out.println(tab(weight, values, n, bagCapacity));
     }
 }
